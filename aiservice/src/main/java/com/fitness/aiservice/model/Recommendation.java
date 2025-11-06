99b3decd42708d47825cae6d8e29cfdb58eea86b
htrix <htrix@example.com>
2025-11-06 17:17:01 +0000

    Add config server module with initial setup and configuration files

--- aiservice/src/main/java/com/fitness/aiservice/model/Recommendation.java
+++ aiservice/src/main/java/com/fitness/aiservice/model/Recommendation.java
@@ -1,9 +1,7 @@
 package com.fitness.aiservice.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -14,14 +12,12 @@ import java.util.List;
 @Document(collection = "recommendations")
 @Data
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class Recommendation {
     @Id
     private String id;
     private String activityId;
+    private String type;
     private String userId;
-    private String activityType;
     private String recommendation;
     private List<String> improvements;
     private List<String> suggestions;
